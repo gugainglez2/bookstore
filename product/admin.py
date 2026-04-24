2174670f8cebb83fea0a6173f9cfb50894926593
Gustavo Inglez <gugainglez@gmail.com>
2026-04-24 01:10:32 -0300

    Adiciona docker-compose

--- product/admin.py
+++ product/admin.py
@@ -3,3 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Product, Category
+from django.contrib import admin
+
+admin.site.register(Product)
+admin.site.register(Category)
